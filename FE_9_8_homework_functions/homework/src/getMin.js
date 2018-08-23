e4b9e81a75a555c945e69c8984033477cdfda76f
pavel fabuliak <pavlo.fabuliak@gmail.com>
2018-08-23 16:27:37 +0300

    Refactored

--- FE_9_8_homework_functions/homework/src/getMin.js
+++ FE_9_8_homework_functions/homework/src/getMin.js
@@ -4,15 +4,12 @@ function getMin(){
 	
     for (let i = 1; i < array.length; i++){
         if(array[i-1] > array[i]) {
-			stepBack(i, array);
+			for( ; i > 0 && array[i-1] > array[i]; i--){
+				let swap = array[i];
+				array[i] = array[i-1];
+				array[i-1] = swap;
+			}
 		}
-	}
-	function stepBack(i, array){
-        for( ; i > 0 && array[i-1] > array[i]; i--){
-            let swap = array[i];
-            array[i] = array[i-1];
-            array[i-1] = swap;
-        }
 	}
     return array[0];
 }
