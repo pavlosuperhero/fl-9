fd0f54b283dae05ba871ce66881ab1a37a52d0cb
pavel fabuliak <pavlo.fabuliak@gmail.com>
2018-08-23 16:10:45 +0300

    Function getMin reworked, GnomSort algorithm

--- FE_9_8_homework_functions/homework/src/getMin.js
+++ FE_9_8_homework_functions/homework/src/getMin.js
@@ -1,4 +1,18 @@
-// Your code goes here
-function getMin() {
-	return Math.min.apply(... arguments);
+
+function getMin(){
+	let array = [...arguments];
+	
+    for (let i = 1; i < array.length; i++){
+        if(array[i-1] > array[i]) {
+			stepBack(i, array);
+		}
+	}
+	function stepBack(i, array){
+        for( ; i > 0 && array[i-1] > array[i]; i--){
+            let swap = array[i];
+            array[i] = array[i-1];
+            array[i-1] = swap;
+        }
+	}
+    return array[0] === 0 ? array[1] : array[0];
 }
